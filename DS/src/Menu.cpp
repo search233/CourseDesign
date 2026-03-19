619694b3e33c06fdca8b0719144b11ab7e92165d
YangZ <pterocaryz@outlook.com>
2026-03-20 00:12:30 +0800

    更改文件路径

--- DS/src/Menu.cpp
+++ DS/src/Menu.cpp
@@ -2,8 +2,8 @@
 #include "FileManager.h"
 #include <iostream>
 
-const std::string city_file = "~/Projects/CourseDesign/DS/data/cities.txt";
-const std::string edge_file = "~/Projects/CourseDesign/DS/data/edges.txt";
+const std::string city_file = "data/cities.txt";
+const std::string edge_file = "data/edges.txt";
 
 Menu::Menu() {
     run_tag = 1;
