74b6cb7463826b12e350cda3c2549de3ab378432
yangz <pterocaryz@outlook.com>
2026-09-21 12:01:57 +0800

    update utils.cc

---
+++ nw/src/core/utils.cc
@@ -0,0 +1,81 @@
+#include "utils.h"
+
+ethernet::ErrorCode
+    utils::parse_mac (
+        const std::string& mac_str,
+        std::vector<uint8_t>& mac,
+        bool is_dest = true) {
+
+    // 0: success
+    // 1: invalid dest
+    // 2: invalid src
+    unsigned int tag = 0;
+    auto put_tag = [&]() -> void {
+        if (is_dest) tag = 1;
+        else tag = 2;
+    }
+
+    int len = mac_str.length();
+    std::string clean_str;
+    for (int i = 0; i < len; ++i) {
+        char ch = mac_str[i];
+
+        if (ch == '-' || ch == ':' || ch == ' ') {
+            if ((i + 1) % 3 == 0) continue;
+            eles {
+                put_tag();
+                break;
+            }
+        }
+
+        if (!std::isxdigit(static_cast<unsigned char>(ch))) {
+            put_tag();
+            break;
+        }
+
+        clean_str += ch;
+    }
+
+    if (clean_str.length() != 12) {
+        put_tag();
+    }
+
+    switch (tag) {
+        using err_code = ethernet::ErrorCode;
+        case 0: {
+            return err_code::Success;
+        }
+
+        case 1: {
+            return err_code::InvalidDestMac;
+        }
+
+        case 2: {
+            return err_code::InvalidSrcMac;
+        }
+
+        default: {
+            assert(tag <= 2);
+        }
+    }
+}
+
+std::string utils::byte_to_hex (uint8_t byte) {
+    std::ostringstream oss;
+
+    oss << std::hex 
+        << std::uppercase 
+        << std::setfill('0') 
+        << std::setw(2) 
+        << static_cast<int>(byte);
+
+    return oss.str();
+}
+
+std::string 
+    utils::mac_to_string (const std::array<uint8_t, 8>& mac) {
+    std::ostringstream oss;
+
+    for (auto)
+
+}
--- nw/src/core/utils.h
+++ nw/src/core/utils.h
@@ -7,9 +7,13 @@ namespace utils {
 
 // 解析字符串，得到一个6字节mac地址
 ethernet::ErrorCode 
-    parse_mac (const std::string& mac_str, 
-            std::vector<uint8_t>& mac,
-            bool is_dest = true);
+    parse_mac 
+        (const std::string& mac_str, 
+        std::vector<uint8_t>& mac,
+        bool is_dest = true);
+
+// 将单个字节转化成大写的16进制串
+std::string byte_to_hex(uint8_t byte);
 
 // 将mac地址转换成字符串
 std::string 
@@ -22,6 +26,5 @@ std::vector<uint8_t> ascii_to_bytes (const std::string& ascii_str);
 std::string byte_to_bit_string(uint8_t byte);
 std::string twobyte_to_bitstring (uint16_t byte);
 
-// 将字节转化成大写的16进制串
-std::string byte_to_hex(uint8_t byte);
+
 }
